0310995749f3b6d99a857a3dae4c67946dbae4ef
Golam Morshed <45533293+GolamMorshed@users.noreply.github.com>
2023-11-20 17:01:41 +0000

    push code

---
+++ app/Http/Controllers/InternalStaffController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InternalStaff;
+use Illuminate\Http\Request;
+
+class InternalStaffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InternalStaff $internalStaff)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InternalStaff $internalStaff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, InternalStaff $internalStaff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InternalStaff $internalStaff)
+    {
+        //
+    }
+}
